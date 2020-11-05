d97969f1fb77215fb3259a78f73c265692a28c35
Adrian Nesvik <adrian.nesvik@lyse.net>
2020-11-05 09:30:14 +0100

    Add external api call

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+const { fetchByDates } = require('./api');
 
-app.get('/', function (req, res) {
-  res.send("test")
+app.get('/api/', async (req, res) => {
+  const data = await fetchByDates('2014-01-01', '2014-01-02')
+  res.send(data)
+});
+
+app.get('/', (req, res) => {
+  res.send('test')
 });
 
 app.listen(5000, (console.log('Server is running on port 5000...')));
